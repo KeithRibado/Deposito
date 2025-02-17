8a94a07e06c3ce1e946470ad481bd3fdaa4b7bea
KeithRibado <informaticakeith@gmail.com>
2025-02-17 20:12:17 +0000

    Segundo commit: cambios en Main

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -16,7 +16,7 @@ public class Main {
  double saldoActual;
  cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
  saldoActual = cuenta1.estado();
- System.out.println("El saldo actual es "+ saldoActual );
+ System.out.println("El saldo actualmente es "+ saldoActual );
         operaciones(cuenta1);
 
  saldoActual = cuenta1.estado();
@@ -27,6 +27,7 @@ public class Main {
         // Operaciones de ingreso y retiro
         cuenta1.retirar(2300);
         cuenta1.ingresar(685);
+        System.out.println("¿Desea realizar otra operacion?");
     }
 }
 
